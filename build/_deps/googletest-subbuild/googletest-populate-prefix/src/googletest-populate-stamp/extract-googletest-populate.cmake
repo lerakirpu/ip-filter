7b9aaba3a6288afa6500f46d770d489f82f07187
Lera Kirpu <lerakirpu@mail.ru>
2025-11-24 00:57:46 +0700

    com

--- build/_deps/googletest-subbuild/googletest-populate-prefix/src/googletest-populate-stamp/extract-googletest-populate.cmake
+++ build/_deps/googletest-subbuild/googletest-populate-prefix/src/googletest-populate-stamp/extract-googletest-populate.cmake
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
 # file LICENSE.rst or https://cmake.org/licensing for details.
 
@@ -6,7 +5,7 @@ cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
 
 # Make file names absolute:
 #
-get_filename_component(filename "C:/Users/user/Documents/lab2/build/_deps/googletest-subbuild/googletest-populate-prefix/src/03597a01ee50ed33e9fdab60b14b31dbd6d83ad9.zip" ABSOLUTE)
+get_filename_component(filename "C:/Users/user/Documents/lab2/build/_deps/googletest-subbuild/googletest-populate-prefix/src/03597a01ee50ed33e9fdabf8a2def5d696d7a9d2.zip" ABSOLUTE)
 get_filename_component(directory "C:/Users/user/Documents/lab2/build/_deps/googletest-src" ABSOLUTE)
 
 message(VERBOSE "extracting...
@@ -64,70 +63,3 @@ message(VERBOSE "extracting... [clean up]")
 file(REMOVE_RECURSE "${ut_dir}")
 
 message(VERBOSE "extracting... done")
-=======
-# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
-# file LICENSE.rst or https://cmake.org/licensing for details.
-
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-# Make file names absolute:
-#
-get_filename_component(filename "C:/Users/user/Documents/lab2/build/_deps/googletest-subbuild/googletest-populate-prefix/src/03597a01ee50ed33e9fdab60b14b31dbd6d83ad9.zip" ABSOLUTE)
-get_filename_component(directory "C:/Users/user/Documents/lab2/build/_deps/googletest-src" ABSOLUTE)
-
-message(VERBOSE "extracting...
-     src='${filename}'
-     dst='${directory}'"
-)
-
-if(NOT EXISTS "${filename}")
-  message(FATAL_ERROR "File to extract does not exist: '${filename}'")
-endif()
-
-# Prepare a space for extracting:
-#
-set(i 1234)
-while(EXISTS "${directory}/../ex-googletest-populate${i}")
-  math(EXPR i "${i} + 1")
-endwhile()
-set(ut_dir "${directory}/../ex-googletest-populate${i}")
-file(MAKE_DIRECTORY "${ut_dir}")
-
-# Extract it:
-#
-message(VERBOSE "extracting... [tar xfz]")
-execute_process(COMMAND ${CMAKE_COMMAND} -E tar xfz ${filename} 
-  WORKING_DIRECTORY ${ut_dir}
-  RESULT_VARIABLE rv
-)
-
-if(NOT rv EQUAL 0)
-  message(VERBOSE "extracting... [error clean up]")
-  file(REMOVE_RECURSE "${ut_dir}")
-  message(FATAL_ERROR "Extract of '${filename}' failed")
-endif()
-
-# Analyze what came out of the tar file:
-#
-message(VERBOSE "extracting... [analysis]")
-file(GLOB contents "${ut_dir}/*")
-list(REMOVE_ITEM contents "${ut_dir}/.DS_Store")
-list(LENGTH contents n)
-if(NOT n EQUAL 1 OR NOT IS_DIRECTORY "${contents}")
-  set(contents "${ut_dir}")
-endif()
-
-# Move "the one" directory to the final directory:
-#
-message(VERBOSE "extracting... [rename]")
-file(REMOVE_RECURSE ${directory})
-get_filename_component(contents ${contents} ABSOLUTE)
-file(RENAME ${contents} ${directory})
-
-# Clean up:
-#
-message(VERBOSE "extracting... [clean up]")
-file(REMOVE_RECURSE "${ut_dir}")
-
-message(VERBOSE "extracting... done")
->>>>>>> a4ababa8710bc826c16bc5a79746d6e3b73e7b15
